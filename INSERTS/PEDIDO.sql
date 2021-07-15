623df4ec378fed27f5c58555ca320636c028752f
Joshep27 <flowersjoshep@gmail.com>
2021-07-15 17:47:33 -0400

    Update Pedido

---
+++ INSERTS/PEDIDO.sql
@@ -0,0 +1,20 @@
+
+
+-------------PEDIDOS PAIS Nº1-----------------------------------------------
+INSERT INTO PEDIDO VALUES (1,1,5,1000,'EN ENVIADO',null,1,Fecha('04-SEP-2021','04-SEP-2021'),Existencia(80,100),null);
+INSERT INTO PEDIDO VALUES (2,1,5,1000,'EN PROCESO',null,1,Fecha('04-SEP-2021','04-SEP-2021'),Existencia(30,80),null);
+INSERT INTO PEDIDO VALUES (3,1,4,2000,'EN PROCESO',null,5,Fecha('04-SEP-2021','04-SEP-2021'),Existencia(80,100),null);
+INSERT INTO PEDIDO VALUES (4,1,6,2000,'EN PROCESO',null,4,Fecha('04-SEP-2021','04-SEP-2021'),Existencia(20,200),40);
+
+
+---------------PEDIDOS PAIS Nº2-------------------------------------------------------
+INSERT INTO PEDIDO VALUES (5,2,5,1000,'EN PROCESO',null,3,Fecha('04-SEP-2021','04-SEP-2021'),Existencia(80,200),null);
+INSERT INTO PEDIDO VALUES (6,2,4,2000,'EN PROCESO',null,4,Fecha('04-SEP-2021','04-SEP-2021'),Existencia(80,100),null);
+INSERT INTO PEDIDO VALUES (7,2,6,2000,'EN PROCESO',null,1,Fecha('04-SEP-2021','04-SEP-2021'),Existencia(20,200),20);
+
+
+-------------PEDIDOS PAIS Nº3-----------------------------------------------
+INSERT INTO PEDIDO VALUES (8,3,5,1000,'EN PROCESO',null,1,Fecha('04-SEP-2021','04-SEP-2021'),Existencia(80,100),null);
+INSERT INTO PEDIDO VALUES (9,3,8,1000,'EN PROCESO',null,1,Fecha('04-SEP-2021','04-SEP-2021'),Existencia(30,80),null);
+INSERT INTO PEDIDO VALUES (10,3,4,2000,'ENVIADO',null,5,Fecha('04-SEP-2021','04-SEP-2021'),Existencia(80,100),null);
+
